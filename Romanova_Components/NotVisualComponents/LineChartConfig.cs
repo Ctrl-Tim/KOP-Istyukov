daa681a1f6745ed170e1b165ec5710f6a807ba7f
Ctrl-Tm <73001228+Ctrl-Tim@users.noreply.github.com>
2022-11-16 18:29:27 +0400

    romanova_components

---
+++ Romanova_Components/NotVisualComponents/LineChartConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotVisualComponents
+{
+    public class LineChartConfig
+    {
+        public string FilePath { get; set; }
+        public string Header { get; set; }
+        public string ChartTitle { get; set; }
+        public Dictionary<string, List<int>> Values { get; set; }
+        public LegendPosition LegendPosition { get; set; }
+    }
+}
---
+++ Romanova_Components/lab/RomanovaComboBox.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab
+{
+    public partial class RomanovaComboBox : UserControl
+    {
+        public event EventHandler IndexChanged;
+
+        public RomanovaComboBox()
+        {
+            InitializeComponent();
+            comboBox.SelectedIndexChanged += (sender, e) => IndexChanged?.Invoke(sender, e);
+        }
+
+        public void FillList(string item)
+        {
+            comboBox.Items.Add(item);
+        }
+
+        public void ClearList()
+        {
+            comboBox.Items.Clear();
+            comboBox.Text = "";
+        }
+
+        public string SelectElement
+        {
+            get
+            {
+                if (comboBox.SelectedItem == null)
+                {
+                    return string.Empty;
+                }
+                return comboBox.SelectedItem.ToString();
+            }
+            set
+            {
+                comboBox.SelectedItem = value;
+            }
+        }
+
+        public event EventHandler SelectIndexChanged
+        {
+
+            add
+            {
+                IndexChanged += value;
+            }
+            remove
+            {
+                IndexChanged -= value;
+            }
+        }
+    }
+}
