ddcb9452e757497c9c1f9e4cee2a272ec534fd3a
Ctrl-Tm <73001228+Ctrl-Tim@users.noreply.github.com>
2022-11-17 12:16:39 +0400

    create business logics

---
+++ LibraryBusinessLogic/BusinessLogics/BookLogic.cs
@@ -0,0 +1,69 @@
+﻿using LibraryContracts.BusinessLogicsContracts;
+using LibraryContracts.StorageContracts;
+using LibraryContracts.BindingModels;
+using LibraryContracts.ViewModels;
+
+namespace LibraryBusinessLogic.BusinessLogics
+{
+    public class BookLogic : IBookLogic
+    {
+        private readonly IBookStorage _bookStorage;
+
+        public BookLogic(IBookStorage bookStorage)
+        {
+            _bookStorage = bookStorage;
+        }
+
+        public List<BookViewModel> Read(BookBindingModel model)
+        {
+            if (model == null)
+            {
+                return _bookStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<BookViewModel> { _bookStorage.GetElement(model) };
+            }
+            return _bookStorage.GetFilteredList(model);
+        }
+
+        public void CreateOrUpdate(BookBindingModel model)
+        {
+            var element = _bookStorage.GetElement(
+                new BookBindingModel
+                {
+                    Title = model.Title,
+                    Shape = model.Shape,
+                    Annotation = model.Annotation,
+                    Reader1 = model.Reader1,
+                    Reader2 = model.Reader2,
+                    Reader3 = model.Reader3,    
+                    Reader4 = model.Reader4,
+                    Reader5 = model.Reader5,
+                    Reader6 = model.Reader6
+                });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Книга с таким названием уже существует");
+            }
+            if (model.Id.HasValue)
+            {
+                _bookStorage.Update(model);
+            }
+            else
+            {
+                _bookStorage.Insert(model);
+            }
+        }
+
+        public void Delete(BookBindingModel model)
+        {
+            var element = _bookStorage.GetElement(new BookBindingModel { Id = model.Id });
+            if (element == null)
+            {
+                throw new Exception("Книга не найдена");
+            }
+            _bookStorage.Delete(model);
+        }
+    }
+}
---
+++ LibraryBusinessLogic/BusinessLogics/ShapeLogic.cs
@@ -0,0 +1,61 @@
+﻿using LibraryContracts.BusinessLogicsContracts;
+using LibraryContracts.StorageContracts;
+using LibraryContracts.BindingModels;
+using LibraryContracts.ViewModels;
+
+namespace LibraryBusinessLogic.BusinessLogics
+{
+    public class ShapeLogic : IShapeLogic
+    {
+        private readonly IShapeStorage _shapeStorage;
+
+        public ShapeLogic(IShapeStorage shapeStorage)
+        {
+            _shapeStorage = shapeStorage;
+        }
+
+        public List<ShapeViewModel> Read(ShapeBindingModel model)
+        {
+            if (model == null)
+            {
+                return _shapeStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<ShapeViewModel> { _shapeStorage.GetElement(model) };
+            }
+            return _shapeStorage.GetFilteredList(model);
+        }
+
+        public void CreateOrUpdate(ShapeBindingModel model)
+        {
+            var element = _shapeStorage.GetElement(
+                new ShapeBindingModel
+                {
+                    Name = model.Name
+                });
+            if (element != null && element.Id != model.Id)
+            {
+                throw new Exception("Такая форма уже существует");
+            }
+            if (model.Id.HasValue)
+            {
+                _shapeStorage.Update(model);
+            }
+            else
+            {
+                _shapeStorage.Insert(model);
+            }
+        }
+
+        public void Delete(ShapeBindingModel model)
+        {
+            var element = _shapeStorage.GetElement(new ShapeBindingModel { Id = model.Id });
+            if (element == null)
+            {
+                throw new Exception("Форма не найдена");
+            }
+            _shapeStorage.Delete(model);
+        }
+    }
+}
