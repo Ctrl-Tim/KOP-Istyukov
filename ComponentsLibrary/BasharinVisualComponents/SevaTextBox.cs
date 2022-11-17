fa201a1684539aa5a38ca6f0a410db9aa48dd513
Ctrl-Tm <73001228+Ctrl-Tim@users.noreply.github.com>
2022-11-17 16:01:24 +0400

    add basharin components

---
+++ ComponentsLibrary/BasharinVisualComponents/SevaTextBox.cs
@@ -0,0 +1,47 @@
+﻿namespace ComponentsLibrary.BasharinVisualComponents
+{
+    public partial class SevaTextBox : UserControl
+    {
+        public int? MaxLenght { get; set; }
+        public int? MinLenght { get; set; }
+        private string text { get; set; }
+        private string Success = "Верно!";
+        private string Wrong = "Не верно!";
+        private string error = string.Empty;
+        public SevaTextBox()
+        {
+            InitializeComponent();
+            textBox1.TextChanged += (sender, e) => _event?.Invoke(sender, e);
+        }
+        private event EventHandler _event;
+        public event EventHandler EnterTextChanged
+        {
+            add
+            {
+                _event += value;
+            }
+            remove
+            {
+                _event -= value;
+            }
+        }
+        public string SelectText
+        {
+            get
+            {
+                if ((MaxLenght != null && MinLenght != null) && textBox1.Text.Length > MinLenght && textBox1.Text.Length < MaxLenght)
+                        return textBox1.Text;
+                else
+                {
+                    MessageBox.Show("Не соответствует диапозону", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return null;
+                }
+            }
+            set
+            {
+                if ((MaxLenght != null && MinLenght != null) && value.Length > MinLenght && value.Length < MaxLenght)
+                    textBox1.Text = value;
+            }
+        }
+    }
+}
